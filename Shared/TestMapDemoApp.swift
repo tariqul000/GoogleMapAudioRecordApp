4614c5d9cf2ac8106b314d36331586afd6a82e12
simec mac-01 <tariqul1993@gmail.com>
2021-11-11 17:12:02 +0600

    Initial Commit

---
+++ Shared/TestMapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMapDemoApp.swift
+//  Shared
+//
+//  Created by Md Tariqul Islam on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TestMapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
